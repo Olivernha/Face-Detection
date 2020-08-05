a8c4a83a982062206c4235bf1ff47e28dfecf165
oliver <nayhtetaung201733@gmail.com>
2020-08-06 00:58:18 +0630

    Added

--- script.js
+++ script.js
@@ -5,22 +5,61 @@ Promise.all([
     faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
     faceapi.nets.ageGenderNet.loadFromUri('./models')
 ]).then(initVideo);
-function initVideo(){
-    navigator.mediaDevices.getUserMedia({  //to open video
+
+
+// function initVideo(){
+//     navigator.mediaDevices.getUserMedia({  //to open video
         
-        video:{
+//         video:{
            
-            width:450,
-            height:520
-    }
-    })
-    .then(function(stream){
-        video.srcObject=stream; 
-    })
-    .catch((error)=>{
-        console.log(error);
-    });
+//             width:450,
+//             height:520
+//     }
+//     })
+//     .then(function(stream){
+//         video.srcObject=stream; 
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+//     });
+// }
+
+// Older browsers might not implement mediaDevices at all, so we set an empty object first
+
+function initVideo(){
+    if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+      }
+      if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function(constraints) {
+          var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+      
+          if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+          }
+      
+          return new Promise(function(resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+          });
+        }
+      }
+      
+      navigator.mediaDevices.getUserMedia({ audio: false, video: { width:450,height:520} })
+      .then(function(stream) {
+        var video = document.querySelector('video');
+        // Older browsers may not have srcObject
+        if ("srcObject" in video) {
+          video.srcObject = stream;
+        } else {
+          // Avoid using this in new browsers, as it is going away.
+          video.src = window.URL.createObjectURL(stream);
+        }
+      })
+      .catch(function(err) {
+        console.log(err.name + ": " + err.message);
+      });
 }
+
 
 video.addEventListener('play',function(){
    
